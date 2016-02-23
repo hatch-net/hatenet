2fb0f8a88b370022aeba6e4ad85fe78137d47f06
zpzhao <zhaozp@uxsino.com>
2016-02-23 10:46:20 +0800

    Update testmain.cpp
    add comments

--- hatchat/src/test/testmain.cpp
+++ hatchat/src/test/testmain.cpp
@@ -1,5 +1,9 @@
+/******************************************
+ * for test git 
+ * **************************************/
 #include <iostream>
 using namespace std;
+
 int main(int argc, char *argv[])
 {
 	cout<<"test for github"<<endl;
